611027fed2434d679a9877524ccda9c8d116733b
gaolanot <>
2019-04-17 10:53:30 -0400

    Primera versión Taller 23 People Tomando como guía: http://acodigo.blogspot.com/2017/02/spring-tutorial-de-introduccion.html

---
+++ src/main/java/com/golano/spring/introduccionspring/HelloService.java
@@ -0,0 +1,15 @@
+/*
+ * Implementación Ejemplo Introducción Spring
+ * http://acodigo.blogspot.com/2017/02/spring-tutorial-de-introduccion.html
+ * Creación de Interface para Servicio Hello
+ */
+package com.golano.spring.introduccionspring;
+
+/**
+ * 16/04/2019
+ * @author gaolanot
+ */
+public interface HelloService {
+    //Metodo saludar del servicio para el ejemplo
+    void saludar();
+}
---
+++ src/main/java/com/golano/spring/introduccionspring/HelloServiceImpl.java
@@ -0,0 +1,18 @@
+/*
+ * Implementación Ejemplo Introducción Spring
+ * http://acodigo.blogspot.com/2017/02/spring-tutorial-de-introduccion.html
+ * Creación de Clase para Servicio Hello \ implementa de HelloService
+ */
+package com.golano.spring.introduccionspring;
+
+/**
+ * 16/04/2019
+ * @author gaolanot
+ */
+public class HelloServiceImpl implements HelloService{
+    //Implementación método saludar
+    @Override
+    public void saludar() {
+            
+    }
+}
